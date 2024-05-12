e2ed18200170cc73cec29a5205165514b9173469
Siddhant <siddhantrsen73938@gmail.com>
2024-05-12 23:27:16 +0530

    Contact form Home page

--- src/components/shared/ContactForm.tsx
+++ src/components/shared/ContactForm.tsx
@@ -11,12 +11,12 @@ const ContactForm = () => {
             <div>
                 <Image src={image} width={450} height={450} alt="logo" />
             </div>
-            <div className="flex flex-col w-full space-y-2 bg-blue-100 min-h-[280px] justify-center p-4">
+            <div className="flex flex-col w-full space-y-2 bg-background min-h-[280px] justify-center p-4 rounded-md">
                 <div>
-                    <h1 className='text-gray-900 text-xl font-medium'>Get in touch!</h1>
-                    <p className='text-gray-900 text-sm'>Stay up-to-date with the latest events with us every time !</p>
+                    <h1 className='text-gray-900 dark:text-white text-xl font-medium'>Get in touch!</h1>
+                    <p className='text-gray-900 dark:text-white text-sm'>Stay up-to-date with the latest events with us every time !</p>
                 </div>
-                <Label htmlFor="email" className='text-gray-900'>Email</Label>
+                <Label htmlFor="email" className='text-gray-900  dark:text-white'>Email</Label>
                 <div className='flex w-full items-center space-x-2'>
                     <Input type="email" className='min-w-[250px]' placeholder="Email" />
                     <Button variant='outline' type="submit">Subscribe</Button>
@@ -25,7 +25,7 @@ const ContactForm = () => {
                     <p className='text-gray-500 text-sm'>Enter your email adress</p>
                 </div>
                 <div>
-                    <p className='text-gray-900 font-semibold text-sm'>By signing up for the newsletter, you agree to our <a className='text-blue-700 hover:underline transition' href="" target="_blank" rel="noopener noreferrer"> Terms of Service and Privacy Policy</a> .</p>
+                    <p className='text-gray-900  dark:text-white font-semibold text-sm'>By signing up for the newsletter, you agree to our <a className='text-blue-700 hover:underline transition' href="" target="_blank" rel="noopener noreferrer"> Terms of Service and Privacy Policy</a> .</p>
                 </div>
             </div>
         </article>
